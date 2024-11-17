77d72e102de18c08ad4f53ad4b0d393e22ec32a8
Krabs <vovastr0v@yandex.ru>
2024-11-17 22:43:07 +0500

    init controllers: event, schedule, tag

---
+++ URFU_Scheduling/Controllers/TagController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace URFU_Scheduling.Controllers
+{
+    public class TagController : Controller
+    {
+        // tag/
+        [HttpPost]
+        public async Task<IActionResult> TagCreate()
+        {
+            // UserId = authcontext.User
+            // response.body.Name
+            // response.body.Color
+            return Ok();
+        }
+
+        // tag/{tagId}/
+        [HttpGet]
+        public async Task<IActionResult> TagRetrieve(int tagId)
+        {
+            // UserId = authcontext.User
+            return Ok("tag obj");
+        }
+
+        // tag/{tagId}/
+        [HttpPut]
+        public async Task<IActionResult> TagUpdate(int tagId)
+        {
+            // UserId = authcontext.User
+            // response.body.Name
+            // response.body.Color
+            return Ok();
+        }
+
+        // tag/{tagId}/
+        [HttpDelete]
+        public async Task<IActionResult> TagDelete(int tagId)
+        {
+            // UserId = authcontext.User
+            return Ok();
+        }
+
+        // tag/
+        [HttpGet]
+        public async Task<IActionResult> TagList()
+        {
+            // UserId = authcontext.User
+            return Ok("all tag obj");
+        }
+    }
+}
