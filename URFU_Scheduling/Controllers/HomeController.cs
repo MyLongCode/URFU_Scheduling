271e23bb422b9942eedb8cada72524a44e36d567
Krabs <vovastr0v@yandex.ru>
2024-11-18 20:23:21 +0500

    pr

--- URFU_Scheduling/Controllers/HomeController.cs
+++ URFU_Scheduling/Controllers/HomeController.cs
@@ -28,5 +28,6 @@ namespace URFU_Scheduling.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
