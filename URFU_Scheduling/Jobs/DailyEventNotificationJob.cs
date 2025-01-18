34d5dfe72ee767823cbaa232f7bd5924ac17c6ee
Maksim Kalugin <gglolgg889@gmail.com>
2025-01-19 00:42:22 +0500

    create base scheduler
    sends a log every 30 seconds about today's events for all users

---
+++ URFU_Scheduling/Jobs/DailyEventNotificationJob.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.CodeAnalysis.Operations;
+using Quartz;
+using URFU_Scheduling_lib.Domain.Repositories;
+using URFU_Scheduling.Services.Interfaces;
+using URFU_Scheduling_lib.Domain.Entities;
+
+namespace URFU_Scheduling.Jobs
+{
+    public class DailyEventNotificationJob : IJob
+    {
+        private readonly IEventService _eventService;
+        private readonly IUserService _userService;
+        private readonly IScheduleService _scheduleService;
+        private readonly ILogger<DailyEventNotificationJob> _logger;
+
+        public DailyEventNotificationJob(
+                IEventService eventService,
+                IUserService userService,
+                IScheduleService scheduleService,
+                ILogger<DailyEventNotificationJob> logger
+                )
+        {
+            _eventService = eventService;
+            _userService = userService;
+            _scheduleService = scheduleService;
+            _logger = logger;
+        }
+
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            _logger.LogInformation("DailyEventNotificationJob started");
+            var today = DateTime.Today;
+            var schedules = _scheduleService.GetAll()
+                .Where(schedule => schedule.Events.Any(ev => ev.DateStart.Date.Equals(today.Date)));
+            Send(schedules, today);
+            _logger.LogInformation("DailyEventNotificationJob finished");
+        }
+
+        public void Send(IEnumerable<Schedule> schedules, DateTime today)
+        {
+            if (!schedules.Any())
+            {
+                _logger.LogInformation("No schedules found.");
+
+            }
+            else
+            {
+                _logger.LogInformation($"Found {schedules.Count()} schedules for today");
+                foreach (Schedule schedule in schedules)
+                {
+                    var user = _userService.Get(schedule.UserId);
+                    var events = schedule.Events.Where(ev => ev.DateStart.Date.Equals(today));
+                    _logger.LogInformation($"{schedule.Name}{user.Login}");
+                    foreach (Event todayEvent in events)
+                    {
+                        _logger.LogInformation($"{todayEvent.Name} at {todayEvent.DateStart} to {todayEvent.DateStart + todayEvent.Duration}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ URFU_Scheduling/Jobs/QuartzHostedService.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.Hosting;
+using Quartz;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace URFU_Scheduling.Jobs
+{
+    public class QuartzHostedService : IHostedService
+    {
+        private readonly IScheduler _scheduler;
+        private readonly ILogger<QuartzHostedService> _logger;
+
+
+        public QuartzHostedService(IScheduler scheduler, ILogger<QuartzHostedService> logger)
+        {
+            _scheduler = scheduler;
+            _logger = logger;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+          _logger.LogInformation("QuartzHostedService starting.");
+            try
+            {
+                  var job = JobBuilder.Create<DailyEventNotificationJob>()
+                      .WithIdentity("DailyEventNotificationJob", "NotificationGroup")
+                       .Build();
+
+                     var trigger = TriggerBuilder.Create()
+                       .WithIdentity("DailyEventNotificationTrigger", "NotificationGroup")
+                        .WithCronSchedule("0/30 * * * * ?") // Запуск каждую секунду
+                       .Build();
+
+                    await _scheduler.ScheduleJob(job, trigger, cancellationToken);
+                     _logger.LogInformation("Job Scheduled.");
+                    await _scheduler.Start(cancellationToken);
+                     _logger.LogInformation("Scheduler started.");
+
+            }
+            catch (Exception ex)
+            {
+               _logger.LogError(ex, "Error starting scheduler.");
+            }
+
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("QuartzHostedService stopping.");
+            if(_scheduler == null)
+            {
+                _logger.LogInformation("Scheduler is null.");
+                return;
+            }
+            if (_scheduler.IsStarted)
+            {
+                 await _scheduler.Shutdown(cancellationToken);
+                 _logger.LogInformation("Scheduler shutdown.");
+            }
+             else
+            {
+                 _logger.LogInformation("Scheduler is not started.");
+            }
+        }
+    }
+}
